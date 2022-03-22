3b2b323df5fb8b3f41e23ee330b5117c6e8cc1ec
kz370 <94254886+kz370@users.noreply.github.com>
2022-03-22 15:47:04 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -18,3 +18,18 @@ arrangedContacts = (array) => {
         return newArray
     })
 }
+
+//convert timestamp to date format dd-mm-yyyy hourr:minutes am/pm
+const timeStampToDate = (date) => {
+    date = new Date(+date)
+    const [month, day] = date.toLocaleDateString().split('/')
+    const year = date.getFullYear();
+    const [hr, mn] = date.toLocaleTimeString().split(':')
+    const dateString = `${day}-${month}-${year}`
+    let hrs
+    hr > 12 ? (hrs = hr - 12) : hrs = hr;
+    hrs < 10 ? hrs = `0${hrs}` : null
+    const timeString = `${hrs}:${mn} ${hr > 12 ? "pm" : "am"}`
+    const fullDate = `${dateString} ${timeString}`
+    return fullDate
+}
