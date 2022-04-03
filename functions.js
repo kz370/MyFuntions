e4a9c086e975d5b8ef5b087c107f85c29257615d
kz370 <94254886+kz370@users.noreply.github.com>
2022-04-03 20:46:09 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -25,11 +25,10 @@ const timeStampToDate = (date) => {
     const [month, day] = date.toLocaleDateString().split('/')
     const year = date.getFullYear();
     const [hr, mn] = date.toLocaleTimeString().split(':')
+    const hrs = hr>12?(hr%12>0?hr%12:12):(hr>0?(hr<10?`0${hr}`:hr):12)
     const dateString = `${day}-${month}-${year}`
-    let hrs
-    hr > 12 ? (hrs = hr - 12) : hrs = hr;
-    hrs < 10 ? hrs = `0${hrs}` : null
     const timeString = `${hrs}:${mn} ${hr > 12 ? "pm" : "am"}`
-    const fullDate = `${dateString} ${timeString}`
+    const fullDate = { date: dateString, time: timeString, fullDate: `${dateString} ${timeString}` }
+
     return fullDate
 }
